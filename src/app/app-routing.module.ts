7e40f9ffaade73a713a5ec60bfc949b297bdb25a
Wilfrido Bonett <wbonett10@gmail.com>
2020-05-07 13:49:28 -0500

    I Create variables and navigation component content

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+/* Components */
+import { HomeMovieComponent } from './features/feature-movie/pages/home-movie/home-movie.component';
+import { NewMovieComponent } from './features/feature-movie/pages/new-movie/new-movie.component';
+import { TopMovieComponent } from './features/feature-movie/pages/top-movie/top-movie.component';
+import { NoPageFoundComponent } from './core/components/no-page-found/no-page-found.component';
 
-const routes: Routes = [];
+/* Routes */
+const routes: Routes = [
+  { path: '', redirectTo: '/movies', pathMatch: 'full' },
+  { path: 'movies', component: HomeMovieComponent },
+  { path: 'new_movie', component: NewMovieComponent },
+  { path: 'top_movies', component: TopMovieComponent },
+
+  { path: '**', component: NoPageFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,10 @@ import { NavbarComponent } from './core/components/navbar/navbar.component';
 import { HomeMovieComponent } from './features/feature-movie/pages/home-movie/home-movie.component';
 import { TopMovieComponent } from './features/feature-movie/pages/top-movie/top-movie.component';
 import { NewMovieComponent } from './features/feature-movie/pages/new-movie/new-movie.component';
+import { MovieDetailComponent } from './features/feature-movie/components/movie-detail/movie-detail.component';
+import { MovieFormComponent } from './features/feature-movie/components/movie-form/movie-form.component';
+import { MovieListComponent } from './features/feature-movie/components/movie-list/movie-list.component';
+import { NoPageFoundComponent } from './core/components/no-page-found/no-page-found.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +22,11 @@ import { NewMovieComponent } from './features/feature-movie/pages/new-movie/new-
     NavbarComponent,
     HomeMovieComponent,
     TopMovieComponent,
-    NewMovieComponent
+    NewMovieComponent,
+    MovieDetailComponent,
+    MovieFormComponent,
+    MovieListComponent,
+    NoPageFoundComponent
   ],
   imports: [
     BrowserModule,
