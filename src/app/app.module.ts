9521fa27fc756f0977e923450fb68525a4c2fda1
Wilfrido Bonett <wbonett10@gmail.com>
2020-05-07 18:26:20 -0500

    refactor on modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,37 +8,21 @@ import { SharedModule } from './shared/shared.module';
 
 /* Components */
 import { AppComponent } from './app.component';
-import { SharedButtonComponent } from './shared/components/shared-button/shared-button.component';
-import { NavbarComponent } from './core/components/navbar/navbar.component';
-import { HomeMovieComponent } from './features/feature-movie/pages/home-movie/home-movie.component';
-import { TopMovieComponent } from './features/feature-movie/pages/top-movie/top-movie.component';
-import { NewMovieComponent } from './features/feature-movie/pages/new-movie/new-movie.component';
-import { MovieDetailComponent } from './features/feature-movie/components/movie-detail/movie-detail.component';
-import { MovieFormComponent } from './features/feature-movie/components/movie-form/movie-form.component';
-import { MovieListComponent } from './features/feature-movie/components/movie-list/movie-list.component';
-import { NoPageFoundComponent } from './core/components/no-page-found/no-page-found.component';
 
 /* Services */
 import { StorageService } from './core/services/storage.service';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SharedButtonComponent,
-    NavbarComponent,
-    HomeMovieComponent,
-    TopMovieComponent,
-    NewMovieComponent,
-    MovieDetailComponent,
-    MovieFormComponent,
-    MovieListComponent,
-    NoPageFoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     InMemoryWebApiModule.forRoot(StorageService),
+    CoreModule,
     SharedModule
   ],
   providers: [],
